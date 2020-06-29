5f9104dcb2f60a98baf1743661d47486dbb752d6
takagimeow <dev@takagi.app>
2020-06-29 17:21:31 +0900

    Ink Boilerplateのベース

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,41 @@
-var path = require('path');
+var path = require("path")
 
 module.exports = {
-    // Change to your "entry-point".
-    mode: "development",
-    entry: './src/index',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json']
-    },
-    module: {
-        rules: [{
-            // Include ts, tsx, js, and jsx files.
-            test: /\.(ts|js)x?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }],
-    }
-};
+	// Change to your "entry-point".
+	mode: "development",
+	entry: "./src/index",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "app.bundle.js",
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js", ".json"],
+	},
+	module: {
+		rules: [
+			{
+				// Include ts, tsx, js, and jsx files.
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+				options: {
+					presets: [
+						"@babel/preset-react",
+						[
+							"@babel/preset-env",
+							{
+								targets: {
+									node: true,
+								},
+							},
+						],
+					],
+					plugins: [
+						"@babel/proposal-class-properties",
+						"@babel/proposal-object-rest-spread",
+					],
+				},
+			},
+		],
+	},
+}
